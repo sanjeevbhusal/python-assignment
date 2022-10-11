b03eeff5cd92dcef282d619d8c997596b3f6c350
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-11 11:17:55 +0545

    added functionlity to fetch 30 articles per script run

--- hello.py
+++ hello.py
@@ -3,47 +3,47 @@ from requests.exceptions import RequestException
 import json
 import sys
 
-POSTS_PER_PAGE = 21
+ARTICLE_PER_PAGE = 10
+ARTICLES_TO_FETCH = 30
 
 
-def fetch_news(current_page):
-    if current_page > 1:
-        print(f"You already have some news fetched. Program will start fetching from page {current_page}")
-
-    print("Fetching News....")
-
-    url = f"https://bg.annapurnapost.com/api/news/list?page={current_page}&per_page=21&category_alias=sports" \
-          f"&isCategoryPage=1 "
+def fetch_articles(current_page):
+    url = f"https://bg.annapurnapost.com/api/news/list?page={current_page}&per_page={ARTICLE_PER_PAGE}\
+    &category_alias=sports"f"&isCategoryPage=1 "
     try:
         response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     except RequestException:
         sys.exit("Oops! Some Error occurred while fetching the Post.")
     else:
+        print(f"Successfully Fetched {current_page * 10} articles")
         return response.text
 
 
-def get_existing_news():
+def get_existing_articles():
     with open("news.json", "a+") as f:
         f.seek(0)
         news = f.read()
         return json.loads(news) if len(news) > 0 else []
 
 
-def get_news():
-    existing_news = get_existing_news()
-    existing_news_length = len(existing_news)
-    page_to_fetch = (int(existing_news_length / POSTS_PER_PAGE)) + 1
-    html = fetch_news(page_to_fetch)
-    news = json.loads(html)["data"]
-    updated_news = news + existing_news
-    return json.dumps(updated_news)
+def get_articles():
+    total_articles_fetched = 0
+    while total_articles_fetched < ARTICLES_TO_FETCH:
+        existing_articles = get_existing_articles()
+        page_to_fetch = (int(len(existing_articles) / ARTICLE_PER_PAGE)) + 1
+
+        html = fetch_articles(page_to_fetch)
+        articles = json.loads(html)["data"]
+        updated_articles = articles + existing_articles
+        total_articles_fetched += ARTICLE_PER_PAGE
+
+        with open("news.json", "w") as f:
+            f.write(str(json.dumps(updated_articles)))
 
 
 def main():
-    news_list = get_news()
-    with open("news.json", "w") as f:
-        f.write(str(news_list))
-
+    print("Fetching News....")
+    get_articles()
     print('News have been fetched. Check file called "news.json". ')
 
 
