d6feb43f67ea4859658d2d64a9efabcf75bd4ef8
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2022-10-11 09:46:07 +0545

    added display message

--- hello.py
+++ hello.py
@@ -7,6 +7,11 @@ POSTS_PER_PAGE = 21
 
 
 def fetch_news(current_page):
+    if current_page > 1:
+        print(f"You already have some news fetched. Program will start fetching from page {current_page}")
+
+    print("Fetching News....")
+
     url = f"https://bg.annapurnapost.com/api/news/list?page={current_page}&per_page=21&category_alias=sports" \
           f"&isCategoryPage=1 "
     try:
@@ -18,7 +23,8 @@ def fetch_news(current_page):
 
 
 def get_existing_news():
-    with open("news.json", "r") as f:
+    with open("news.json", "a+") as f:
+        f.seek(0)
         news = f.read()
         return json.loads(news) if len(news) > 0 else []
 
@@ -27,7 +33,6 @@ def get_news():
     existing_news = get_existing_news()
     existing_news_length = len(existing_news)
     page_to_fetch = (int(existing_news_length / POSTS_PER_PAGE)) + 1
-
     html = fetch_news(page_to_fetch)
     news = json.loads(html)["data"]
     updated_news = news + existing_news
@@ -38,6 +43,8 @@ def main():
     news_list = get_news()
     with open("news.json", "w") as f:
         f.write(str(news_list))
+
+    print('News have been fetched. Check file called "news.json". ')
 
 
 if __name__ == "__main__":
